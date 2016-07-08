19e1d053b53485807c64d1a7e54fcb2ef14d8957
Alexey Verein <alexey@plainvanillagames.com>
2016-07-08 17:55:57 +0000

    Add copyright

--- app/src/main/java/com/antonioleiva/bandhookkotlin/data/mapper/AlbumMapper.kt
+++ app/src/main/java/com/antonioleiva/bandhookkotlin/data/mapper/AlbumMapper.kt
@@ -1,15 +1,25 @@
+/*
+ * Copyright (C) 2016 Alexey Verein
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package com.antonioleiva.bandhookkotlin.data.mapper
 
 import com.antonioleiva.bandhookkotlin.data.lastfm.model.LastFmAlbum
 import com.antonioleiva.bandhookkotlin.data.lastfm.model.LastFmAlbumDetail
 import com.antonioleiva.bandhookkotlin.domain.entity.Album
 import com.antonioleiva.bandhookkotlin.domain.entity.Artist
-
-/**
- * @author tpom6oh@gmail.com
- *
- * 03/07/16.
- */
 
 class AlbumMapper(val artistMapper: ArtistMapper = ArtistMapper(), val imageMapper: ImageMapper = ImageMapper(),
                   val trackMapper: TrackMapper = TrackMapper()) {
--- app/src/main/java/com/antonioleiva/bandhookkotlin/ui/presenter/AlbumPresenter.kt
+++ app/src/main/java/com/antonioleiva/bandhookkotlin/ui/presenter/AlbumPresenter.kt
@@ -1,3 +1,19 @@
+/*
+ * Copyright (C) 2016 Alexey Verein
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package com.antonioleiva.bandhookkotlin.ui.presenter
 
 import com.antonioleiva.bandhookkotlin.domain.interactor.GetAlbumDetailInteractor
@@ -7,11 +23,6 @@ import com.antonioleiva.bandhookkotlin.domain.interactor.event.AlbumEvent
 import com.antonioleiva.bandhookkotlin.ui.entity.mapper.AlbumDetailDataMapper
 import com.antonioleiva.bandhookkotlin.ui.view.AlbumView
 
-/**
- * @author tpom6oh@gmail.com
- *
- * 05/07/16.
- */
 open class AlbumPresenter(
         override val view: AlbumView,
         override val bus: Bus,
