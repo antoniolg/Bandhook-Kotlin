66f527ca88aa8e3a47fe523ed682e66d7e3a4373
Antonio Leiva <antonioleivag@gmail.com>
2017-03-05 14:43:24 +0100

    Use AppBarLayout for scroll effects on MainActivity

--- app/src/main/java/com/antonioleiva/bandhookkotlin/ui/screens/main/MainLayout.kt
+++ app/src/main/java/com/antonioleiva/bandhookkotlin/ui/screens/main/MainLayout.kt
@@ -1,13 +1,20 @@
 package com.antonioleiva.bandhookkotlin.ui.screens.main
 
+import android.support.design.widget.AppBarLayout
+import android.support.design.widget.AppBarLayout.LayoutParams.*
 import android.support.v7.widget.RecyclerView
 import android.support.v7.widget.Toolbar
 import android.view.ViewGroup.LayoutParams.MATCH_PARENT
 import com.antonioleiva.bandhookkotlin.R
 import com.antonioleiva.bandhookkotlin.ui.activity.ActivityAnkoComponent
 import com.antonioleiva.bandhookkotlin.ui.custom.autoFitRecycler
-import org.jetbrains.anko.*
+import org.jetbrains.anko.AnkoContext
 import org.jetbrains.anko.appcompat.v7.toolbar
+import org.jetbrains.anko.backgroundResource
+import org.jetbrains.anko.design.appBarLayout
+import org.jetbrains.anko.design.coordinatorLayout
+import org.jetbrains.anko.dip
+import org.jetbrains.anko.topPadding
 
 class MainLayout : ActivityAnkoComponent<MainActivity> {
 
@@ -15,19 +22,27 @@ class MainLayout : ActivityAnkoComponent<MainActivity> {
     override lateinit var toolbar: Toolbar
 
     override fun createView(ui: AnkoContext<MainActivity>) = with(ui) {
-        frameLayout {
+
+        coordinatorLayout {
+
+            appBarLayout {
+                toolbar = toolbar(R.style.ThemeOverlay_AppCompat_Dark_ActionBar) {
+                    backgroundResource = R.color.primary
+                    fitsSystemWindows = true
+                    popupTheme = R.style.ThemeOverlay_AppCompat_Light
+                }.lparams(width = MATCH_PARENT) {
+                    scrollFlags = SCROLL_FLAG_SNAP or SCROLL_FLAG_SCROLL or SCROLL_FLAG_ENTER_ALWAYS
+                }
+            }.lparams(width = MATCH_PARENT)
+
             recycler = autoFitRecycler {
-                topPadding = dip(56)
                 clipToPadding = false
                 columnWidth = dip(180)
 
-            }.lparams(MATCH_PARENT, MATCH_PARENT)
+            }.lparams(MATCH_PARENT, MATCH_PARENT) {
+                behavior = AppBarLayout.ScrollingViewBehavior()
+            }
 
-            toolbar = toolbar(R.style.ThemeOverlay_AppCompat_Dark_ActionBar) {
-                backgroundResource = R.color.primary
-                fitsSystemWindows = true
-                popupTheme = R.style.ThemeOverlay_AppCompat_Light
-            }.lparams(width = MATCH_PARENT)
         }
     }
 }
