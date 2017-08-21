3525fbe0b4ba214e5a8770f66d03811d917e7b79
Víctor González <victor.gonzalez.irimia@gmail.com>
2017-08-21 12:34:16 +0200

    Upgrade Retrofit to 2.3.0, and deal with nullable body responses.

--- app/src/main/java/com/antonioleiva/bandhookkotlin/data/Util.kt
+++ app/src/main/java/com/antonioleiva/bandhookkotlin/data/Util.kt
@@ -2,4 +2,4 @@ package com.antonioleiva.bandhookkotlin.data
 
 import retrofit2.Call
 
-inline fun <T, U> Call<T>.unwrapCall(f: T.() -> U) = execute().body().f()
+inline fun <T, U> Call<T>.unwrapCall(f: T.() -> U) = execute().body()?.f()
